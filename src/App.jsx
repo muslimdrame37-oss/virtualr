55873db90cc10da437896cad9ffcd9a937c619de
Muslim Drame <muslimdrame37@gmail.com>
2026-06-13 08:15:50 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
+import FeatureSection from "./constants/FeatureSection";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+
+      </div>
+    </>
+  );
+}
+
+export default App;
